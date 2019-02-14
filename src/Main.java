ac5102e92dcf5fc160975c4eac42fefef8bdf1a7
Nechemia Seif <nechemiaseif@gmail.com>
2019-02-14 11:56:19 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author neche
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        String oneWrong = "wrong";
+        String anotherWrong = "wrong";
+        String aRight = "right";
+
+        if ((oneWrong + anotherWrong).equals(aRight)) {
+            System.out.println("Two wrongs make a right.");
+        } else {
+            System.out.println("Two wrongs don't make a right.");
+        }
+
+    }
+
+}
+
+
+/*
+Ugh, I hate template garbage!
+ */
+ /*
+Ugh, I hate template garbage!
+ */
+ /*
+Ugh, I hate template garbage!
+ */
+ /*
+Ugh, I hate template garbage!
+ */
+ /*
+Ugh, I hate template garbage!
+ */
+ /*
+Ugh, I hate template garbage!
+ */
+ /*
+Ugh, I hate template garbage!
+ */
+ /*
+Ugh, I hate template garbage!
+ */
